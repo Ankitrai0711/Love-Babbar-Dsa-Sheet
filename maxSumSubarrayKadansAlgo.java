1e854e18afad286941047cf3e365c7e4bc79af6d
Ankit Kumar Rai <ankitrai3453@gmail.com>
2025-08-28 18:06:52 +0530

    Update maxSumSubarrayKadansAlgo.java
    adding the another approach in the code

--- maxSumSubarrayKadansAlgo.java
+++ maxSumSubarrayKadansAlgo.java
@@ -11,3 +11,21 @@ public class maxSumSubarrayKadansAlgo {
         System.out.println(max);
     }
 }
+
+//  We also can write code as like this , both are following the concept of the Kadanse` algo
+// class Main{
+//     public static void main(String []args){
+//         int arr[] = {1,2,3,4,5};
+//         int n = arr.length;
+//         int currentSum=0;
+//         int max = Integer.MIN_VALUE;
+//         for(int i =0;i<n;i++){
+//             currentSum = currentSum+arr[i];
+//             max = Math.max(currentSum,max);
+//             if(currentSum < 0){
+//                 currentSum =0;
+//             }
+//         }
+//         System.out.println(max);
+//     }  
+// }
